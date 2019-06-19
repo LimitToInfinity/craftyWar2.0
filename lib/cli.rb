68be27461e2fab6865780126231a09317ee97cd8
github username <github email address>
2019-06-19 10:00:16 -0600

    Code cleaning and formatting.

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,16 +4,23 @@ def main_menu
     case user_input
         when "1"
             if User.all.size > 0
-            existing_user
-            else puts ""
+                system("clear")
+                existing_user
+            else 
+                puts ""
                 puts "There are no users!"
+                system("clear")
                 main_menu
             end
         when "2"
+            system("clear")
             new_user
         when "3"
+            system("clear")
             exit_message
-        else main_menu
+        else 
+            system("clear")
+            main_menu
     end
 end
 
@@ -37,8 +44,11 @@ def existing_user
         user_name.downcase == user.name.downcase
     end
     if user_match
+        system("clear")
         existing_user_menu(user_match)
-    else main_menu
+    else 
+        system("clear")
+        main_menu
     end
 end
 
@@ -47,6 +57,7 @@ def new_user
     puts "Choose your user name:"
     new_user_name = gets.strip
     new_user = User.create(name: new_user_name)
+    system("clear")
     existing_user_menu(new_user)
 end
 
@@ -66,24 +77,31 @@ def existing_user_menu(user_match)
     user_input = gets.strip
     case user_input
         when "1"
+            system("clear")
             character_creation(user_match)
         when "2"
             if user_match.characters.size > 0
+                system("clear")
                 character_menu(user_match)
-            else puts ""
+            else 
+                system("clear") 
+                puts ""
                 puts "You don't have any characters!"
                 existing_user_menu(user_match)
             end
         when "3"
+            system("clear")
             main_menu
         when "4"
+            system("clear")
             exit_message
-        else existing_user_menu(user_match)
+        else 
+            existing_user_menu(user_match)
     end
 end
 
 def character_creation(user_match)
-
+    
     puts ""
     puts "Enter Character name:"
     new_character_name = gets.strip
@@ -105,46 +123,43 @@ def character_creation(user_match)
     
     new_character = Character.create(name: new_character_name, race: new_race, character_class: new_character_class, profession: new_profession, user: user_match)
    
+    system("clear")
     character_option_menu(new_character)
 end
 
 def get_race
+    puts ""
     puts "Choose your Race:"
     user_input = gets.strip
     race_match = Race.all.find {|race| race.name.downcase == user_input.downcase}
     if !race_match
         get_race
-    else race_match
+    else 
+        race_match
     end
 end
 
 def get_character_class
+    puts ""
     puts "Choose your Class:"
     user_input = gets.strip
     character_class_match = CharacterClass.all.find {|character_class| character_class.name.downcase == user_input.downcase}
     if !character_class_match
         get_character_class
-    else character_class_match
+    else 
+        character_class_match
     end
 end
 
 def get_profession
+    puts ""
     puts "Choose your Profession:"
     user_input = gets.strip
     profession_match = Profession.all.find {|profession| profession.name.downcase == user_input.downcase}
     if !profession_match
         get_profession
-    else profession_match
-    end
-end
-
-def get_character_name(user_match)
-    puts "Choose your Character:"
-    user_input = gets.strip
-    character_match = user_match.characters.find {|character| character.name.downcase == user_input.downcase}
-    if !character_match
-        get_character_name(user_match)
-    else character_match
+    else 
+        profession_match
     end
 end
 
@@ -152,10 +167,21 @@ def character_menu(user_match)
     puts ""
     puts "Your characters:"
     puts user_match.characters.map {|character| puts character.name}.compact
-    puts ""
-    puts "Enter Character name:"
     character_choice = get_character_name(user_match)
+    system("clear")
     character_option_menu(character_choice)
+end
+
+def get_character_name(user_match)
+    puts ""
+    puts "Choose your Character:"
+    user_input = gets.strip
+    character_match = user_match.characters.find {|character| character.name.downcase == user_input.downcase}
+    if !character_match
+        get_character_name(user_match)
+    else 
+        character_match
+    end
 end
 
 def character_option_menu(character_choice)
@@ -175,8 +201,10 @@ def character_option_menu(character_choice)
             new_name = gets.strip
             puts ""
             character_choice.update(name: new_name)
+            system("clear")
             character_option_menu(character_choice)
         when "2"
+            system("clear")
             puts ""
             puts "Character - #{character_choice.name}"
             puts "Race - #{character_choice.race.name}"
@@ -189,15 +217,23 @@ def character_option_menu(character_choice)
             puts "Are you sure? y/n"
             if gets.strip.downcase == "y"
                 character_choice.destroy
+                system("clear")
                 existing_user_menu(character_choice.user)
-            else character_option_menu(character_choice)
+            else 
+                system("clear")
+                character_option_menu(character_choice)
             end
         when "4"
+            system("clear")
             existing_user_menu(character_choice.user)
         when "5"
+            system("clear")
             main_menu
         when "6"
+            system("clear")
             exit_message
-        else character_option_menu(character_choice)
+        else 
+            system("clear")
+            character_option_menu(character_choice)
     end
 end
